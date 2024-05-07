43866fbdf8791da5f4bc315953198d153fe9ecde
AdamChen <powq2011@gmail.com>
2024-05-07 11:04:51 +0800

    add file explore demo item

---
+++ app/src/main/java/com/adam/app/demoset/filemanager/FileExploreAct.java
@@ -0,0 +1,147 @@
+package com.adam.app.demoset.filemanager;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.adam.app.demoset.R;
+import com.adam.app.demoset.Utils;
+import com.adam.app.demoset.databinding.ActivityDemoDataBindingExBinding;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class FileExploreAct extends AppCompatActivity {
+
+    static final int MANAGE_EXTERNAL_STORAGE_PERMISSION_REQUEST = 1;
+    static final int READ_EXTERNAL_STORAGE_PERMISSION_REQUEST = 2;
+
+    private ActivityDemoDataBindingExBinding mBinding;
+
+    private List<File> mFileList;
+    private ArrayAdapter<String> mStrAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        info("onCreate");
+
+        this.mBinding = ActivityDemoDataBindingExBinding.inflate(getLayoutInflater());
+        this.mBinding.toolbar.inflateMenu(R.menu.action_menu_file_manager);
+        setContentView(this.mBinding.getRoot());
+
+        setupUi();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        info("onResume");
+        // check file explore permission
+        boolean hasPermission = FileUtils.checkStoragePermission(this);
+        info("hasPermission: " + String.valueOf(hasPermission));
+        // show ui according to permission state
+        if (!hasPermission) {
+            this.mBinding.rationaleView.setVisibility(View.VISIBLE);
+            this.mBinding.filesTreeView.setVisibility(View.GONE);
+            return;
+        }
+
+        //check sdk version
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+            if (!Environment.isExternalStorageLegacy()) {
+                info("!Environment.isExternalStorageLegacy()");
+                this.mBinding.rationaleView.setVisibility(View.GONE);
+                this.mBinding.legacyStorageView.setVisibility(View.VISIBLE);
+                return;
+            }
+        }
+
+        this.mBinding.rationaleView.setVisibility(View.GONE);
+        this.mBinding.filesTreeView.setVisibility(View.VISIBLE);
+        // OPEN
+        open(Environment.getExternalStorageDirectory());
+    }
+
+    private void setupUi() {
+        info("setupUi");
+        // set menu action
+        this.mBinding.toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                // start about activity
+                Utils.showToast(FileExploreAct.this, "No implement yet!!!");
+                return true;
+            }
+        });
+
+        // set permission button
+        this.mBinding.permissionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileUtils.requestStoragePermission(FileExploreAct.this);
+            }
+        });
+
+        // set list action
+        this.mStrAdapter =new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        this.mBinding.filesTreeView.setAdapter(this.mStrAdapter);
+        this.mBinding.filesTreeView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                File selectedItem = FileExploreAct.this.mFileList.get(position);
+                open(selectedItem);
+            }
+        });
+    }
+
+    private void open(File selectedItem) {
+        info("open");
+        // check is file or not
+        if (selectedItem.isFile()) {
+            FileUtils.openFile(this, selectedItem);
+            return;
+        }
+
+        // update list view
+        boolean hasFile = true;
+        try {
+            this.mFileList = FileUtils.getFiles(selectedItem);
+        } catch (FileNotFoundException e) {
+            Utils.showToast(this, "No file!!!");
+            hasFile = false;
+        }
+
+        if (!hasFile) {
+            // no file so do not need to open!!!
+            return;
+        }
+
+        // clear adapter
+        this.mStrAdapter.clear();
+        this.mStrAdapter.addAll(mapItems(selectedItem));
+
+    }
+
+    private List<String> mapItems(File seletctedFile) {
+        info("mapItems");
+        return this.mFileList.stream()
+                .map(file -> file.getPath().equals(Objects.requireNonNull(seletctedFile.getParentFile()).getPath()) ? FileUtils.ParentLinkLabel(this) : FileUtils.ItemLabel(this, file))
+                .collect(Collectors.toList());
+    }
+
+    private void info(String msg) {
+        Utils.info(this, msg);
+    }
+
+}
---
+++ app/src/main/java/com/adam/app/demoset/filemanager/FileUtils.java
@@ -0,0 +1,182 @@
+package com.adam.app.demoset.filemanager;
+
+
+import android.Manifest;
+import android.app.AppOpsManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.webkit.MimeTypeMap;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.adam.app.demoset.R;
+import com.adam.app.demoset.Utils;
+
+abstract class FileUtils {
+
+    private static final String AUTHORITY = "com.adam.app.demoset.filemanager.provider";
+
+    public static final String MANAGE_EXTERNAL_STORAGE_PERMISSION = "android:manage_external_storage";
+    public static final String NOT_APPLICABLE = "N/A";
+
+    public static String getStoragePermissionName() {
+        // check sdk version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+            return MANAGE_EXTERNAL_STORAGE_PERMISSION;
+        return Manifest.permission.READ_EXTERNAL_STORAGE;
+    }
+
+    public static void openPermissionSettings(AppCompatActivity activity) {
+        // check sdk version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            requestStoragePermissionApi30(activity);
+            return;
+        }
+
+        activity.startActivity(
+                new Intent(
+                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.fromParts("package", activity.getPackageName(), null)
+                )
+        );
+    }
+
+    public static String getLegacyStorageStatus() {
+        // check sdk version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+            return String.valueOf(Environment.isExternalStorageLegacy());
+
+        return NOT_APPLICABLE;
+    }
+
+
+    public static String getPermissionStatus(AppCompatActivity activity) {
+        // check sdk version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+            return checkStoragePermissionApi30(activity).toString();
+        return checkStoragePermissionApi19(activity).toString();
+    }
+
+    public static Boolean checkStoragePermission(AppCompatActivity activity) {
+        Utils.info(FileUtils.class, "checkStoragePermission");
+        // check sdk version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R)
+            return checkStoragePermissionApi30(activity);
+        return checkStoragePermissionApi19(activity);
+    }
+
+    public static void requestStoragePermission(AppCompatActivity activity) {
+        // check sdk version
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            requestStoragePermissionApi30(activity);
+            return;
+        }
+
+        requestStoragePermissionApi19(activity);
+    }
+
+    @RequiresApi(30)
+    private static Boolean checkStoragePermissionApi30(AppCompatActivity activity) {
+        AppOpsManager appOps = (AppOpsManager) activity.getSystemService(AppOpsManager.class);
+        int mode = appOps.unsafeCheckOpNoThrow(
+                MANAGE_EXTERNAL_STORAGE_PERMISSION,
+                activity.getApplicationInfo().uid,
+                activity.getPackageName()
+        );
+
+        return mode == AppOpsManager.MODE_ALLOWED;
+    }
+
+    @RequiresApi(30)
+    private static void requestStoragePermissionApi30(AppCompatActivity activity) {
+        Intent intent =new Intent(Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION);
+        // start activity
+        activity.startActivityForResult(intent, FileExploreAct.MANAGE_EXTERNAL_STORAGE_PERMISSION_REQUEST);
+    }
+
+    @RequiresApi(19)
+    private static Boolean checkStoragePermissionApi19(AppCompatActivity activity) {
+        int status = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE);
+        return status == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @RequiresApi(19)
+    private static void requestStoragePermissionApi19(AppCompatActivity activity) {
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+        ActivityCompat.requestPermissions(
+                activity,
+                permissions,
+                FileExploreAct.READ_EXTERNAL_STORAGE_PERMISSION_REQUEST
+        );
+    }
+
+    private static String getMimeType(String uriStr) {
+        String ext = MimeTypeMap.getFileExtensionFromUrl(uriStr);
+        String mime = MimeTypeMap.getSingleton().getMimeTypeFromExtension(ext);
+        return (mime != null)? mime: "text/plain";
+    }
+
+
+    public static void  openFile(AppCompatActivity activity, File selectedItem) {
+        // Get Uri and mime type
+        Uri uri = FileProvider.getUriForFile(activity.getApplicationContext(), AUTHORITY, selectedItem);
+        String mime = getMimeType(uri.toString());
+
+        // Open file with user selected app
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setDataAndType(uri, mime);
+        activity.startActivity(intent);
+    }
+
+
+    public static List<File> getFiles(File selectedItem) throws FileNotFoundException {
+        List<File> resultList = new ArrayList<>();
+        // filter not hidden file in list
+        File[] files = selectedItem.listFiles();
+        if (files == null) {
+            throw new FileNotFoundException("No files!!!");
+        }
+
+        List<File> filterFiles = new ArrayList<>();
+        for (File file: files) {
+            if (!file.isHidden()) {
+                // add
+                filterFiles.add(file);
+            }
+        }
+
+        // is directory?
+        if (selectedItem.equals(Environment.getExternalStorageDirectory())) {
+            return filterFiles;
+        }
+
+        resultList.add(selectedItem.getParentFile());
+        resultList.addAll(filterFiles);
+        return resultList;
+    }
+
+    public static String ParentLinkLabel(AppCompatActivity activity) {
+        return activity.getString(R.string.go_parent_label);
+    }
+
+    public static String ItemLabel(AppCompatActivity activity, File file) {
+        return (file.isDirectory())? activity.getString(R.string.folder_item, file.getName()):
+                activity.getString(R.string.file_item, file.getName());
+    }
+
+
+}
