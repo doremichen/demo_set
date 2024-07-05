a32a27c390aed9465b20da4ac6fba5f7e2e240d0
AdamChen <powq2011@gmail.com>
2024-07-05 09:15:48 +0800

    refactor Femo flash light item

--- app/src/main/java/com/adam/app/demoset/flashlight/FlashLightService.java
+++ app/src/main/java/com/adam/app/demoset/flashlight/FlashLightService.java
@@ -9,6 +9,9 @@ import androidx.core.app.JobIntentService;
 
 import com.adam.app.demoset.Utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FlashLightService extends JobIntentService {
 
     private static final int JOB_ID = 0x0316;
@@ -24,6 +27,61 @@ public class FlashLightService extends JobIntentService {
         JobIntentService.enqueueWork(context, FlashLightService.class, JOB_ID, work);
     }
 
+    /**
+     * Turn on/off Flash light interface
+     */
+    private abstract class FlashLightAction {
+
+        public void handler() {
+            CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+            try {
+                String cameraId = manager.getCameraIdList()[0];
+                manager.setTorchMode(cameraId, isEnabled());
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        protected abstract boolean isEnabled();
+    }
+
+    private class On extends FlashLightAction {
+
+        @Override
+        protected boolean isEnabled() {
+            return true;
+        }
+    }
+
+    private class Off extends FlashLightAction {
+
+        @Override
+        protected boolean isEnabled() {
+            return false;
+        }
+    }
+
+
+    /**
+     * Turn on/off Flash light context
+     */
+    private class FlashFightSwitchContext {
+        private Map<String , FlashLightAction> mMap = new HashMap<>() {
+            {
+                put(CMD_FLASH_LIGHT_ON, new On());
+                put(CMD_FLASH_LIGHT_OFF, new Off());
+            }
+        };
+
+        private void process(String enabled) {
+            // process flash light function
+            this.mMap.get(enabled).handler();
+        }
+
+    }
+
+
 
     @Override
     protected void onHandleWork(@NonNull Intent intent) {
@@ -31,31 +89,9 @@ public class FlashLightService extends JobIntentService {
 
         String cmd = intent.getStringExtra(KEY_COMMEND);
 
-        if (CMD_FLASH_LIGHT_ON.equals(cmd)) {
-
-            enableFlash(true);
-
-        } else if (CMD_FLASH_LIGHT_OFF.equals(cmd)) {
-
-            enableFlash(false);
-
-        }
-
+        new FlashFightSwitchContext().process(cmd);
 
     }
-
-
-    private void enableFlash(boolean on) {
-        Utils.info(this, "startFlash enter");
-        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-
-        try {
-            String cameraId = manager.getCameraIdList()[0];
-            manager.setTorchMode(cameraId, on);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-
-    }
+    
 
 }
