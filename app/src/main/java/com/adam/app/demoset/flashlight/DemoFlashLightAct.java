00b7bc9eabafeab2af5337d2d98ded888929f820
AdamChen <powq2011@gmail.com>
2025-10-03 16:30:10 +0800

    [Demo flash light]: refine demo screen

--- app/src/main/java/com/adam/app/demoset/flashlight/DemoFlashLightAct.java
+++ app/src/main/java/com/adam/app/demoset/flashlight/DemoFlashLightAct.java
@@ -6,33 +6,34 @@ package com.adam.app.demoset.flashlight;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.adam.app.demoset.R;
 import com.adam.app.demoset.Utils;
+import com.adam.app.demoset.databinding.ActivityDemoFlashLightBinding;
 
-public class DemoFlashLightAct extends AppCompatActivity implements FlashLightViewModel.ViewModelCallBack{
+public class DemoFlashLightAct extends AppCompatActivity implements FlashLightViewModel.ViewModelCallBack {
 
     public static final int REQUEST_CAMERA_PERMISSION_CODE = 1;
     public static final String PROP_FLASH_LIGHT_ENABLE = "flash light enable";
-    private ToggleButton mTButton;
-
+    private static final boolean USE_SERVICE = true;
     // flash light view model
     private FlashLightViewModel mFlViewModel;
-
-
-    private static final boolean USE_SERVICE = true;
+    // view binding
+    private ActivityDemoFlashLightBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_demo_flash_light);
+        // view binding
+        mBinding = ActivityDemoFlashLightBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
+        // check flash light
         boolean isFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
         // Ask permission
@@ -44,30 +45,28 @@ public class DemoFlashLightAct extends AppCompatActivity implements FlashLightVi
         if (isFlash) {
             Utils.showToast(this, "the flash light is available");
 
-            mTButton = this.findViewById(R.id.toggleButton_flashlight);
-
             // Check flash light status
             String status = System.getProperty(PROP_FLASH_LIGHT_ENABLE);
 
             if (FlashLightService.CMD_FLASH_LIGHT_ON.equals(status)) {
-                mTButton.setChecked(true);
+                // enable flash in switch
+                mBinding.switchFlashlight.setChecked(true);
             }
 
-            mTButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    boolean isChecked = mTButton.isChecked();
-                    String action = (isChecked == true)? FlashLightService.CMD_FLASH_LIGHT_ON: FlashLightService.CMD_FLASH_LIGHT_OFF;
-                    Utils.showToast(DemoFlashLightAct.this, "action: " + action);
-                    System.setProperty(PROP_FLASH_LIGHT_ENABLE, action);
-                    // enable flash
-                    if (USE_SERVICE) {
-                        FlashLightService.execute(DemoFlashLightAct.this, action);
-                    } else {
-                        mFlViewModel.enableFlashlight(isChecked);
-                    }
+            // set switch listener
+            mBinding.switchFlashlight.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                String action = (isChecked) ? FlashLightService.CMD_FLASH_LIGHT_ON : FlashLightService.CMD_FLASH_LIGHT_OFF;
+                Utils.showToast(DemoFlashLightAct.this, "action: " + action);
+                System.setProperty(PROP_FLASH_LIGHT_ENABLE, action);
+                // enable flash
+                if (USE_SERVICE) {
+                    FlashLightService.execute(DemoFlashLightAct.this, action);
+                } else {
+                    mFlViewModel.enableFlashlight(isChecked);
                 }
             });
+
+
         }
 
     }
