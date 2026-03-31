f5bfcffe5619b88a3412ad6b53aa51f302304777
AdamChen <powq2011@gmail.com>
2026-03-31 15:51:18 +0800

    [Demo Database]: replace AsyncTask with Executor service to execute db operation

--- app/src/main/java/com/adam/app/demoset/database/room/room/NoteRepository.java
+++ app/src/main/java/com/adam/app/demoset/database/room/room/NoteRepository.java
@@ -10,18 +10,21 @@
 package com.adam.app.demoset.database.room.room;
 
 import android.app.Application;
-import android.os.AsyncTask;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 
-import com.adam.app.demoset.utils.Utils;
 import com.adam.app.demoset.database.room.entity.Note;
+import com.adam.app.demoset.utils.Utils;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NoteRepository {
 
+    // Executor service
+    private static final ExecutorService mDbService = Executors.newSingleThreadExecutor();
     private final LiveData<List<Note>> mAllNotes;
     private final NoteDao mNoteDao;
 
@@ -51,70 +54,17 @@ public class NoteRepository {
      */
     public void insert(@NonNull Note note) {
         Utils.info(this, "insert ");
-        new InsertTask(mNoteDao).execute(note);
+        mDbService.execute(() -> mNoteDao.insertNote(note));
     }
 
     public void update(Note... notes) {
         Utils.info(this, "update ");
-        new UpdateTask(mNoteDao).execute(notes);
+        mDbService.execute(() -> mNoteDao.updateNotes(notes));
     }
 
     public void delete(Note... notes) {
         Utils.info(this, "delete ");
-        new DeleteTask(mNoteDao).execute(notes);
-    }
-
-    /**
-     * DB operater task
-     */
-    private static class InsertTask extends AsyncTask<Note, Void, Void> {
-
-        private final NoteDao mDao;
-
-        InsertTask(NoteDao dao) {
-            this.mDao = dao;
-        }
-
-
-        @Override
-        protected Void doInBackground(Note... notes) {
-            Utils.info(this, "doInBackground enter");
-            this.mDao.insertNote(notes[0]);
-            return null;
-        }
-    }
-
-    private static class UpdateTask extends AsyncTask<Note, Void, Void> {
-
-        private final NoteDao mDao;
-
-        UpdateTask(NoteDao dao) {
-            this.mDao = dao;
-        }
-
-
-        @Override
-        protected Void doInBackground(Note... notes) {
-            Utils.info(this, "doInBackground enter");
-            this.mDao.updateNotes(notes);
-            return null;
-        }
-    }
-
-    private static class DeleteTask extends AsyncTask<Note, Void, Void> {
-
-        private final NoteDao mDao;
-
-        DeleteTask(NoteDao dao) {
-            this.mDao = dao;
-        }
-
-        @Override
-        protected Void doInBackground(Note... notes) {
-            Utils.info(this, "doInBackground enter");
-            mDao.deleteNotes(notes[0]);
-            return null;
-        }
+        mDbService.execute(() -> mNoteDao.deleteNotes(notes));
     }
 
 }
